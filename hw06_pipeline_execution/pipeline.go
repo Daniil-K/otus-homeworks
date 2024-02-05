d9f634f8fb4c4040b08659f82bf4cb5812e5a326
D-Klimov <daniilklimov25@gmail.com>
2024-02-05 19:58:22 +0300

    HW 06 Done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	transmitter := func(in Bi, done In, out Out) {
+		for {
+			select {
+			case <-done:
+				close(in)
+				return
+			default:
+				select {
+				case <-done:
+					close(in)
+					return
+				case tmp, ok := <-out:
+					if ok {
+						in <- tmp
+					} else {
+						close(in)
+						return
+					}
+				}
+			}
+		}
+	}
+	for _, stage := range stages {
+		input := make(Bi)
+		go transmitter(input, done, in)
+		in = stage(input)
+	}
+	return in
 }
